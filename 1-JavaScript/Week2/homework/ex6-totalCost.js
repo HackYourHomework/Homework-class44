02a8b454d2d67842c7a7e76ad2c484406600da84
Milad Mike <milad.mike@gmail.com>
2023-05-23 17:26:52 +0200

    Homework(exercise 4, 5, 6 & 7) week2 Milad

--- 1-JavaScript/Week2/homework/ex6-totalCost.js
+++ 1-JavaScript/Week2/homework/ex6-totalCost.js
@@ -2,30 +2,24 @@
 /*------------------------------------------------------------------------------
 Full description at: https://github.com/HackYourFuture/Homework/tree/main/1-JavaScript/Week3#exercise-6-total-cost-is
 
-You want to buy a couple of things from the supermarket to prepare for a party.
-After scanning all the items the cashier wants to give you the total price, but
-the machine is broken! Let's write her a function that does it for her
-instead!
-
-1. Create an object named `cartForParty` with five properties. Each property
-   should be a grocery item (like `beers` or `chips`) and hold a number value
-   (like `1.75` or `0.99`).
-
-2. Complete the function called `calculateTotalPrice`.
-
-   - It takes one parameter: an object that contains properties that only contain
-     number values.
-   - Loop through the object and add all the number values together.
-   - Return a string: "Total: €`amount`".
-
 3. Complete the unit test functions and verify that all is working as expected.
 -----------------------------------------------------------------------------*/
 const cartForParty = {
-  // TODO complete this object
+  beers: 1.0,
+  chips: 3.0,
+  jucies: 2.0,
+  fruits: 5.0,
+  breads: 6.0,
 };
 
-function calculateTotalPrice(/* TODO parameter(s) go here */) {
-  // TODO replace this comment with your code
+function calculateTotalPrice(shoppingCart) {
+  let total = 0;
+
+  for (let item in shoppingCart) {
+    total = total + shoppingCart[item];
+  }
+
+  return 'Total: €' + total.toFixed(2);
 }
 
 // ! Test functions (plain vanilla JavaScript)
