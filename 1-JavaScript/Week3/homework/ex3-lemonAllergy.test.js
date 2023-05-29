9c38864beeb1cf24e698b36cb9119c365256883e
Ahmet Avci <ahmetavci.8873@gmail.com>
2023-05-29 10:04:12 +0200

    all tests passed

--- 1-JavaScript/Week3/homework/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week3/homework/ex3-lemonAllergy.test.js
@@ -26,26 +26,26 @@ const fruitBasket = [
 ];
 
 // ! Function under test
-function sanitizeFruitBasket(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function sanitizeFruitBasket(fruitBasket, fruitToRemove) {
+  return fruitBasket.filter((fruit) => fruit !== fruitToRemove);
 }
 
 // ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(sanitizeFruitBasket).toHaveLength(2);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
-    const originalFruitBasketContents = [...fruitBasket];
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    const originalFruitBasket = [...fruitBasket];
+    sanitizeFruitBasket(fruitBasket, 'lemon');
+    expect(fruitBasket).toEqual(originalFruitBasket);
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    const result = sanitizeFruitBasket(fruitBasket, 'lemon');
+    const expected = ['apple', 'grapefruit', 'banana', 'watermelon'];
+    expect(result).toEqual(expected);
   });
 });
